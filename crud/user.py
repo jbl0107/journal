2e08a9d1d08d7b0f7ad869d14a682ae718f841cd
jbl0107 <ballestero20017@gmail.com>
2025-10-17 05:34:03 +0200

    feat: obtener usuario por id (completo)
    - Faltó el CRUD en el commit anterior

--- crud/user.py
+++ crud/user.py
@@ -1,8 +1,12 @@
 from sqlalchemy import select
 from sqlalchemy.orm import Session
-from models.user import User
+from models.user import User #
 
 
-def get_users(session:Session):
+def get_users(session:Session) -> list[User]:
     return session.scalars(select(User)).all()
 
+
+def get_user_by_id(session:Session, id:int) -> User | None:
+    return session.scalar(select(User).where(User.id == id))
+    
