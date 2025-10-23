9a5ac6c3992a7e2dfb029f502b701cfe068c9e1b
jbl0107 <ballestero20017@gmail.com>
2025-10-23 20:23:30 +0200

    refactor: mantener coherencia entre UserBase y UserPatch
    - Se añade el patrón en el campo username de UserPatch
    
    - Se le da valor por defecto a password

--- schemas/user.py
+++ schemas/user.py
@@ -26,9 +26,9 @@ class UserUpdate(UserCreate):
 class UserPatch(UserBase):
     first_name:Optional[str] = Field(None, min_length=2, max_length=25)
     last_name:Optional[str] = Field(None, min_length=2, max_length=30)
-    username:Optional[str] = Field(None, min_length=3, max_length=20)
+    username:Optional[str] = Field(None, min_length=3, max_length=20, pattern=r'^\S+$')
     age:Optional[int] = Field(None, gt=0, lt=100)
-    password:Optional[str] = Field(min_length=8)
+    password:Optional[str] = Field(None, min_length=8)
 
 
 
