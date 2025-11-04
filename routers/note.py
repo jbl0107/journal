87f9b1e756bebabb1075248363074e65fa5f0b5a
jbl0107 <ballestero20017@gmail.com>
2025-11-04 16:07:45 +0100

    feat: añadir endpoint GET/{id} de Note
    - Obtener una nota por id

--- routers/note.py
+++ routers/note.py
@@ -1,8 +1,8 @@
 from db import get_db
 from schemas.note import NoteRead
-from crud.note import get_notes
+from crud.note import get_notes, get_note_by_id
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 router = APIRouter(prefix='/notes', tags=['Notes'])
@@ -11,3 +11,15 @@ router = APIRouter(prefix='/notes', tags=['Notes'])
 def get_all(session:Session = Depends(get_db)) -> list[NoteRead]:
     '''Devuelve todas las notas creadas por los usuarios'''
     return get_notes(session)
+
+
+@router.get('/{id}', responses={
+    404:{'description':'La nota con id especificado no existe'}
+})
+def get_by_id(id:int, session:Session = Depends(get_db)) -> NoteRead:
+    '''Recupera la información de una nota específica'''
+    note = get_note_by_id(session, id)
+    if note is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='La nota con id especificado no existe')
+    
+    return note
