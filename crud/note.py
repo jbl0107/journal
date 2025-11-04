36c0cbdee9349845d90bbb032e4d1f6f035c49ff
jbl0107 <ballestero20017@gmail.com>
2025-11-04 15:34:11 +0100

    feat: obtener Note por id (CRUD)

--- crud/note.py
+++ crud/note.py
@@ -7,3 +7,11 @@ from sqlalchemy.orm import Session
 def get_notes(session:Session) -> list[Note]:
     ''' Operación CRUD que obtiene todos los usuarios'''
     return session.scalars(select(Note)).all()
+
+
+def get_note_by_id(id:int, session:Session) -> Note | None:
+    '''
+    Operación CRUD que devuelve la Note con id especificado.
+    Si no existe, devuelve None
+    '''
+    return session.get(Note, id)
