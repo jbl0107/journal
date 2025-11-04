2761f897809374dcf6387f8ec059c5a993e0b303
jbl0107 <ballestero20017@gmail.com>
2025-11-04 17:46:48 +0100

    feat: crear Note (CRUD)

--- crud/note.py
+++ crud/note.py
@@ -1,4 +1,5 @@
 from models.note import Note
+from schemas.note import NoteCreate
 
 from sqlalchemy import select
 from sqlalchemy.orm import Session
@@ -15,3 +16,15 @@ def get_note_by_id(session:Session, id:int) -> Note | None:
     Si no existe, devuelve None
     '''
     return session.get(Note, id)
+
+
+def create_note(session:Session, note: NoteCreate) -> Note:
+    '''
+    Operación CRUD que inserta un registro en la tabla Note
+    '''
+    new_note = Note(**note.model_dump())
+
+    with session.begin():
+        session.add(new_note)
+
+    return new_note
